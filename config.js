2e7b287efa6bfb44796eb5c79ab930f609de8549
spa20 <spa20@mails.tsinghua.edu.cn>
2025-06-06 17:43:39 +0800

    update

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // 游戏配置
 export const config = {
   // 内容切换时间间隔（毫秒）
-  contentSwitchInterval: 670,
+  contentSwitchInterval: 660,
   
   // 炼丹炉呼吸动画时间间隔（秒）
-  furnacePulseInterval: 0.67,
+  furnacePulseInterval: 0.66,
   
   // 游戏倒计时（秒）
   gameDuration: 20,
   
   // 正负面物品出现概率
-  positiveItemProbability: 0.65,
+  positiveItemProbability: 0.6,
 }; 
